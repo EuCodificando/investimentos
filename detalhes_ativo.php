5c92dc6d703299fc69791ee5e8e3174f3583765e
EuCodificando <adailton.dssilva@gmail.com>
2024-11-02 09:41:14 -0300

    a

--- detalhes_ativo.php
+++ detalhes_ativo.php
@@ -8,42 +8,6 @@ $_SESSION['ativo'] = $ativo;
 $dados_ativo = $ativos->solicitar_lista_posicoes($ativo);
 $cotacoes_ativo = $ativos->solicitar_cotacoes($ativo);
 var_dump($_GET, $dados_ativo);
-<<<<<<< HEAD
-=======
-$cotacoes_ativo = $iAtivo->solicitar_cotacoes($dados_ativo);
-// var_dump($cotacoes_ativo);exit;
-$retorno_data = $iAtivo->solicitar_data_atualizacao_cotacoes();
-if (is_array($retorno_data) && in_array('erro', array_keys($retorno_data))) {
-    $retorno_ultima_cotacao = $iBanco->obter_ultima_cotacao($dados_ativo)[0];
-    // var_dump($retorno_ultima_cotacao);
-    $retorno_atualizacao = $iBanco->atualizar_data_cotacoes($retorno_ultima_cotacao['data_cotacao']);
-    if ($retorno_atualizacao) {
-        $retorno_data = $iBanco->obter_data_atualizacao_cotacoes();
-        var_dump($retorno_data);
-    }
-
-    // var_dump($retorno_atualizacao);exit;
-}
-$data_atualizacao = new \DateTime($retorno_data['data_atualizacao_cotacoes']);
-$eixo_x = [];
-$eixo_y = [];
-for ($i = 252; $i > 0; $i--) {
-    $eixo_x[] = date_format(new \DateTime($cotacoes_ativo[$i]['data_cotacao']), 'd');
-    $eixo_y[] = $cotacoes_ativo[$i]['fechamento'];
-}
-
-$x = implode("','", $eixo_x);
-$y = implode(',', $eixo_y);
-
-var_dump(count($eixo_x), $x, count($eixo_y), $y);
-$data_atual = new \DateTime('now');
-
-$intervalo = new \DateInterval('P1DT0M');
-
-$intervalo = $data_atualizacao->diff($data_atual);
-$libera_atualizacao_cotacao = $intervalo->days >= 1;
-// var_dump($data);
->>>>>>> 58f7fb5 (Aprendendo echarts)
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -120,12 +84,8 @@ $libera_atualizacao_cotacao = $intervalo->days >= 1;
                         // smooth: true
                         areaStyle: {}
                     },
-<<<<<<< HEAD
-                ]                
-=======
                 ]
                 
->>>>>>> 58f7fb5 (Aprendendo echarts)
             };
             grafico.setOption(option);
         </script>
