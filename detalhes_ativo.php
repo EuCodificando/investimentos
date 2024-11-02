a1dd6d76b04102ab23a995cd9882bbf748614ede
EuCodificando <adailton.dssilva@gmail.com>
2024-11-02 08:54:17 -0300

    Voltar ao main

--- detalhes_ativo.php
+++ detalhes_ativo.php
@@ -23,6 +23,71 @@ var_dump($_GET, $dados_ativo);
 <section>
     <a href="/editar_ativo.php">Editar ativo</a>
 </section>
+    <section>
+        <a href="/">Página inicial</a>
+        <a href="/editar_ativo.php">Editar ativo</a>
+    </section>
+    <?php if (!is_null($info)): ?>
+        <section>
+            <fieldset>
+                <legend>Info</legend>
+                <p><?= $info ?></p>
+            </fieldset>
+        </section>
+        <?php unset($_SESSION['retorno_edicao_ativo']) ?>;
+    <?php endif ?>
+    <section>
+        <p>Data da última cotação: <?= date_format($data_atualizacao, 'd/m/Y') ?></p>
+        <?php if ($libera_atualizacao_cotacao): ?>
+            <a href="/atualizar_cotacoes.php">Atualizar cotacoes</a>
+        <?php endif ?>
+    </section>
+    <section class="grafico">
+        <div id="main" style="width:100%;height:400"></div>
+        <script type="text/javascript">
+
+            var grafico = echarts.init(document.getElementById('main'));
+            window.addEventListener('resize', function () {
+                grafico.resize();
+            });
+            var option;
+
+            option = {
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: ['<?= $x ?>']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                dataZoom: [
+                    {
+                        type: 'inside',
+                        start: 0,
+                        end: 50
+                    },
+                    {
+                        start: 10,
+                        end: 70
+                    }
+                ],
+                series: [
+                    {
+                        name: 'Preço',
+                        data: [<?= $y ?>],
+                        type: 'line',
+                        // smooth: true
+                        areaStyle: {}
+                    },
+                ]                
+            };
+            grafico.setOption(option);
+        </script>
+    </section>
 </body>
 
 </html>
